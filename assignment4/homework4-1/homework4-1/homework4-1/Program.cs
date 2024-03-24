82fe98d36f1b4c91ac52defbe593781294796675
xiangchiyu <1285739457@qq.com>
2024-03-24 16:59:32 +0800

    the 4th homework

---
+++ assignment4/homework4-1/homework4-1/homework4-1/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace homework4_1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { set; get; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head { get => head; }
+        public void Add(T t)
+        {
+            Node <T>n=new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(GenericList<T> list,Action<T> action)
+        {
+            if(action==null) throw new ArgumentNullException("action");
+            Node<T> current = head;
+            while(current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                intlist.Add(i);
+            }
+
+            Action<int> action1 = (m=>Console.WriteLine(m));
+            intlist.ForEach(intlist,action1);
+            int sum = 0;
+            intlist.ForEach(intlist, m=>sum+=m);
+            Console.WriteLine($"sum={sum}");
+
+        }
+    }
+}
---
+++ assignment4/homework4-2/homework4-2/homework4-2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+using System;
+
+namespace homework4_2
+{
+    /*
+     使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+     在闹钟走时时或者响铃时，在控制台显示提示信息。
+     */
+    public delegate void AlarmHandler(object sender, TimeEventArgs args);
+    public delegate void TickHandler(object sender, System.DateTime args);
+    public class TimeEventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+    }
+    public class Clock
+    {
+        private int hour, minute;
+        private TimeEventArgs time;
+        public event AlarmHandler onAlarm;
+        public event TickHandler onTick;
+        public void Alarm(object sender, TimeEventArgs time)
+        {
+            Console.WriteLine($"您设定的时间到啦！");
+        }
+        public void Tick(object sender,System.DateTime time)
+        {
+            Console.WriteLine("滴答");
+        }
+        public Clock(int hour, int minute)
+        {
+            this.hour = hour;this.minute = minute;
+            onTick += Tick;
+            TimeEventArgs args = new TimeEventArgs() { Hour = hour, Minute = minute };
+            onAlarm += Alarm;
+        }
+        public void Start()
+        {
+            while(true)
+            {
+                System.DateTime now= DateTime.Now;
+                onTick(this, now);
+                if (now.Hour == hour && now.Minute == minute)
+                {
+                    onAlarm(this,time);
+                }
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock(16,56);
+            clock.Start();
+        }
+    }
+}
