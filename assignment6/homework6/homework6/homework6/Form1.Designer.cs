79501d4eda7f532f30a7eb15461d127030d786a7
xiangchiyu <1285739457@qq.com>
2024-04-09 15:54:05 +0800

    6

---
+++ assignment6/homework6/homework6/homework6/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace homework6
+{
+    partial class OrderForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.add_button = new System.Windows.Forms.Button();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.sort_button = new System.Windows.Forms.Button();
+            this.choice_text = new System.Windows.Forms.ComboBox();
+            this.search_button = new System.Windows.Forms.Button();
+            this.search_text = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.commodities = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.commodities_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.custom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.goodsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goodsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.add_button);
+            this.flowLayoutPanel1.Controls.Add(this.delete_button);
+            this.flowLayoutPanel1.Controls.Add(this.sort_button);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(588, 27);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(406, 53);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // add_button
+            // 
+            this.add_button.Location = new System.Drawing.Point(3, 3);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(127, 43);
+            this.add_button.TabIndex = 0;
+            this.add_button.Text = "添加";
+            this.add_button.UseVisualStyleBackColor = true;
+            this.add_button.Click += new System.EventHandler(this.add_button_Click);
+            // 
+            // delete_button
+            // 
+            this.delete_button.Location = new System.Drawing.Point(136, 3);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(130, 43);
+            this.delete_button.TabIndex = 1;
+            this.delete_button.Text = "删除";
+            this.delete_button.UseVisualStyleBackColor = true;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // sort_button
+            // 
+            this.sort_button.Location = new System.Drawing.Point(272, 3);
+            this.sort_button.Name = "sort_button";
+            this.sort_button.Size = new System.Drawing.Size(127, 43);
+            this.sort_button.TabIndex = 2;
+            this.sort_button.Text = "排序";
+            this.sort_button.UseVisualStyleBackColor = true;
+            this.sort_button.Click += new System.EventHandler(this.sort_button_Click);
+            // 
+            // choice_text
+            // 
+            this.choice_text.FormattingEnabled = true;
+            this.choice_text.Items.AddRange(new object[] {
+            "id",
+            "price",
+            "custom"});
+            this.choice_text.Location = new System.Drawing.Point(13, 59);
+            this.choice_text.Name = "choice_text";
+            this.choice_text.Size = new System.Drawing.Size(121, 32);
+            this.choice_text.TabIndex = 2;
+            // 
+            // search_button
+            // 
+            this.search_button.Location = new System.Drawing.Point(424, 54);
+            this.search_button.Name = "search_button";
+            this.search_button.Size = new System.Drawing.Size(81, 35);
+            this.search_button.TabIndex = 3;
+            this.search_button.Text = "查询";
+            this.search_button.UseVisualStyleBackColor = true;
+            this.search_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // search_text
+            // 
+            this.search_text.Location = new System.Drawing.Point(164, 57);
+            this.search_text.Name = "search_text";
+            this.search_text.Size = new System.Drawing.Size(232, 35);
+            this.search_text.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(21, 142);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(981, 349);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "订单明细";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.custom,
+            this.commodities,
+            this.commodities_id,
+            this.date,
+            this.price});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(962, 256);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox2.Controls.Add(this.search_text);
+            this.groupBox2.Controls.Add(this.choice_text);
+            this.groupBox2.Controls.Add(this.search_button);
+            this.groupBox2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(21, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1008, 100);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "订单查询";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Customer";
+            this.dataGridViewTextBoxColumn1.HeaderText = "顾客";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 150;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Customer";
+            this.dataGridViewTextBoxColumn2.HeaderText = "顾客";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 150;
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "Id";
+            this.id.HeaderText = "订单号";
+            this.id.MinimumWidth = 8;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 150;
+            // 
+            // commodities
+            // 
+            this.commodities.DataPropertyName = "Name";
+            this.commodities.HeaderText = "商品名";
+            this.commodities.MinimumWidth = 8;
+            this.commodities.Name = "commodities";
+            this.commodities.ReadOnly = true;
+            this.commodities.Width = 150;
+            // 
+            // commodities_id
+            // 
+            this.commodities_id.DataPropertyName = "Id";
+            this.commodities_id.HeaderText = "商品号";
+            this.commodities_id.MinimumWidth = 8;
+            this.commodities_id.Name = "commodities_id";
+            this.commodities_id.ReadOnly = true;
+            this.commodities_id.Width = 150;
+            // 
+            // date
+            // 
+            this.date.DataPropertyName = "CreateTime";
+            this.date.HeaderText = "时间";
+            this.date.MinimumWidth = 8;
+            this.date.Name = "date";
+            this.date.ReadOnly = true;
+            this.date.Width = 150;
+            // 
+            // price
+            // 
+            this.price.DataPropertyName = "TotalPrice";
+            this.price.HeaderText = "金额";
+            this.price.MinimumWidth = 8;
+            this.price.Name = "price";
+            this.price.ReadOnly = true;
+            this.price.Width = 150;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Customer";
+            this.dataGridViewTextBoxColumn3.HeaderText = "顾客";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 150;
+            // 
+            // custom
+            // 
+            this.custom.DataPropertyName = "Customer";
+            this.custom.HeaderText = "顾客";
+            this.custom.MinimumWidth = 8;
+            this.custom.Name = "custom";
+            this.custom.ReadOnly = true;
+            this.custom.Width = 150;
+            // 
+            // orderBindingSource
+            // 
+            this.orderBindingSource.DataSource = typeof(OrderApp.Order);
+            // 
+            // goodsBindingSource
+            // 
+            this.goodsBindingSource.DataSource = typeof(OrderApp.Goods);
+            // 
+            // OrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(1028, 486);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "OrderForm";
+            this.Text = "订单管理";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goodsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button add_button;
+        private System.Windows.Forms.Button delete_button;
+        private System.Windows.Forms.Button sort_button;
+        private System.Windows.Forms.ComboBox choice_text;
+        private System.Windows.Forms.Button search_button;
+        private System.Windows.Forms.TextBox search_text;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource orderBindingSource;
+        private System.Windows.Forms.BindingSource goodsBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn custom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn commodities;
+        private System.Windows.Forms.DataGridViewTextBoxColumn commodities_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+    }
+}
+
---
+++ assignment6/homework6/homework6/homework6/Form2.cs
@@ -0,0 +1,67 @@
+﻿using OrderApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework6
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void admit_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Customer customer = new Customer(Convert.ToInt32(idtext.Text), customtext.Text);
+                Goods goods = new Goods(Convert.ToInt32(goodId_text.Text), goodName_text.Text, Convert.ToInt32(price_text.Text));
+                Order order = new Order(Convert.ToInt32(idtext.Text), customer, DateTime.Now);
+                OrderDetail detail=new OrderDetail(goods, 1);
+                order.AddDetails(detail);
+                OrderForm.orderService.AddOrder(order);
+                MessageBox.Show("添加成功");
+            }
+            catch
+            {
+                MessageBox.Show("请重新输入！");
+            }
+            OrderForm f1 = new OrderForm();
+            this.Hide();
+            f1.ShowDialog();
+            this.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OrderForm.orderService.RemoveOrder(Convert.ToInt32(idtext.Text));
+                MessageBox.Show("删除成功");
+            }
+            catch
+            {
+                MessageBox.Show("请重新输入！");
+            }
+            
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            idtext.Text = null;
+            customtext.Text = null;
+            price_text.Text = null;
+            goodId_text.Text = null;
+            goodName_text.Text = null;
+            time_text.Text = null;
+        }
+    }
+}
